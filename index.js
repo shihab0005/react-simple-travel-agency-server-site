301476bc0ff45d6d5acb197369a744f012e9f870
Your Name <shihab0000000000@gmail.com>
2022-03-05 14:12:12 +0600

    cancel single Package

--- index.js
+++ index.js
@@ -150,6 +150,12 @@ async function run() {
 
         });
 
+        app.delete("/bookingPackage/delete/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query)
+            res.send(result)
+        })
         //get single order 
         app.get("/bookingPackage/single/:id", async (req, res) => {
             const id = req.params.id;
